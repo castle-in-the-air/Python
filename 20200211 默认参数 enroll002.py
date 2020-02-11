6370e7fc11ce5272c717a27ccac1a2d5bba28166
Likie CUI <44593960+castle-in-the-air@users.noreply.github.com>
2020-02-11 21:45:05 +0800

    enroll() 默认参数
    一年级小学生注册的函数，需要传入name和gender两个参数：
    如果要继续传入年龄、城市等信息怎么办？这样会使得调用函数的复杂度大大增加。
    我们可以把年龄和城市设为默认参数：
    这样，大多数学生注册时不需要提供年龄和城市，只提供必须的两个参数：
    只有与默认参数不符的学生才需要提供额外的信息。

---
+++ 20200211 默认参数 enroll002.py
@@ -0,0 +1,20 @@
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city:', city)
+
+'''
+enroll('sarah','F')
+
+输出语句为：
+name: Sarah
+gender: F
+age: 6
+city: Beijing
+'''
+
+'''
+enroll('Bob', 'M', 7)
+enroll('Adam', 'M', city='Tianjin')
+'''
