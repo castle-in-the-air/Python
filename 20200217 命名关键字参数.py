4f5f4357d5a245c00b990afd83a116a44c761f95
Likie CUI <44593960+castle-in-the-air@users.noreply.github.com>
2020-02-17 17:02:14 +0800

    命名关键字参数
    对于关键字参数，函数的调用者可以传入任意不受限制的关键字参数。至于到底传入了哪些，就需要在函数内部通过kw检查。

---
+++ 20200217 命名关键字参数.py
@@ -0,0 +1,8 @@
+def person(name, age, **kw):
+    if 'city' in kw:
+        # 有city参数
+        pass
+    if 'job' in kw:
+        # 有job参数
+        pass
+    print('name:', name, 'age:', age, 'other:', kw)
