6dedfd92f8943762367433da86b71998dc2aec35
Likie CUI <44593960+castle-in-the-air@users.noreply.github.com>
2020-04-12 15:44:12 +0800

    Update 20200411 Print "F".py

--- 20200411 Print "F".py
+++ 20200411 Print "F".py
@@ -1,6 +1,7 @@
-numbers=[5,2,5,2,2]
+numbers = [5, 2, 5, 2, 2]
 for x_count in numbers:
+    #print('x' * x_count)
     output=''
     for count in range(x_count):
-        outout+=''
+        output+='x'
     print(output)
